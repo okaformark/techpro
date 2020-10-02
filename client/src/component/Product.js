1e644965a71ba95722d3cbcb35eb7531bfd56071
okaformark <okaformark@gmail.com>
2020-10-02 01:50:00 -0500

    added fontawesome ratings

--- client/src/component/Product.js
+++ client/src/component/Product.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import Rating from './Rating';
 
 const Product = ({ product }) => {
 	return (
@@ -15,9 +16,10 @@ const Product = ({ product }) => {
 					</Card.Title>
 				</a>
 				<Card.Text as='div'>
-					<div className='my-3'>
-						{product.rating} from {product.numReviewa} reviews
-					</div>
+					<Rating
+						rating={product.rating}
+						text={`${product.numReviews} reviews`}
+					/>
 				</Card.Text>
 				<Card.Text as='h3'>${product.price}</Card.Text>
 			</Card.Body>
