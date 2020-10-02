25c9a2e63e8bc89b47082afd03d9df05631e86c0
okaformark <okaformark@gmail.com>
2020-10-02 02:49:53 -0500

    built individual product page

--- client/src/pages/ProductDetailPage.js
+++ client/src/pages/ProductDetailPage.js
@@ -6,7 +6,68 @@ import products from '../products';
 
 const ProductDetailsPage = ({ match }) => {
 	const product = products.find((product) => product._id == match.params.id);
-	return <div>Product page{product.name}</div>;
+	return (
+		<>
+			<Link className='btn btn-light my-3' to='/'>
+				Return
+			</Link>
+			<Row>
+				<Col md={6}>
+					<Image src={product.image} alt={product.name} fluid />
+				</Col>
+
+				<Col md={3}>
+					<ListGroup variant='flush'>
+						<ListGroup.Item>
+							<h3>{product.name}</h3>
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<Rating>
+								rating ={product.rating} text={product.numReviews}
+							</Rating>
+						</ListGroup.Item>
+						<ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+						<ListGroup.Item>
+							Description: <p>{product.description}</p>
+						</ListGroup.Item>
+					</ListGroup>
+				</Col>
+				<Col md={3}>
+					<Card>
+						<ListGroup variant='flush'>
+							<ListGroup.Item>
+								<Row>
+									<Col>Price:</Col>
+									<Col>
+										<strong>${product.price}</strong>
+									</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>Status:</Col>
+									<Col>
+										<strong>
+											{product.countInStock > 0 ? 'Available' : 'Out of stock'}
+										</strong>
+									</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Button
+									className='btn-block'
+									type='button'
+									disabled={product.countInStock === 0}
+								>
+									ADD TO CART
+								</Button>
+							</ListGroup.Item>
+						</ListGroup>
+					</Card>
+				</Col>
+			</Row>
+		</>
+	);
 };
 
 export default ProductDetailsPage;
