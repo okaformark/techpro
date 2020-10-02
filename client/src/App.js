f46fe9de1ab7e5a6cddc8f377916538422c564f7
okaformark <okaformark@gmail.com>
2020-10-02 00:58:17 -0500

    added navbar and footer with some styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from './component/Footer';
+import Header from './component/Header';
 
 const App = () => {
-	return <></>;
+	return (
+		<>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<h1>WELCOME TO TECH PRO</h1>
+				</Container>
+			</main>
+			<Footer />
+		</>
+	);
 };
 
 export default App;
