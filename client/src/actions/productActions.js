7090e284840e3d66854bb9f90f3838ea3debd2f4
okaformark <okaformark@gmail.com>
2020-10-03 00:14:13 -0500

    single product fetching from DB

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -2,6 +2,9 @@ import {
 	PRODUCT_LIST_REQUEST,
 	PRODUCT_LIST_SUCCESS,
 	PRODUCT_LIST_FAIL,
+	SINGLE_PRODUCT_REQUEST,
+	SINGLE_PRODUCT_SUCCESS,
+	SINGLE_PRODUCT_FAIL,
 } from '../constants/productsConstants';
 import Axios from 'axios';
 
@@ -16,6 +19,24 @@ export const listProduct = () => async (dispatch) => {
 	} catch (error) {
 		dispatch({
 			type: PRODUCT_LIST_FAIL,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
+
+export const listSingleProduct = (id) => async (dispatch) => {
+	try {
+		dispatch({ type: SINGLE_PRODUCT_REQUEST });
+
+		const { data } = await Axios.get(`/api/products/${id}`);
+
+		dispatch({ type: SINGLE_PRODUCT_SUCCESS, payload: data });
+	} catch (error) {
+		dispatch({
+			type: SINGLE_PRODUCT_FAIL,
 			payload:
 				error.response && error.response.data.message
 					? error.response.data.message
