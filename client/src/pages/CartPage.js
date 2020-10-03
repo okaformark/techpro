757c6c618b33f447a910752d68beb72aeb425568
okaformark <okaformark@gmail.com>
2020-10-03 17:57:40 -0500

    items are displayed on cart page and updates on redux

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -27,7 +27,88 @@ const CartPage = ({ match, location, history }) => {
 			dispatch(addToCart(id, quantity));
 		}
 	}, [dispatch, id, quantity]);
-	return <div>cart</div>;
+
+	const removeFromCartHandler = (id) => {};
+
+	return (
+		<Row>
+			<Col md={8}>
+				<h1>Shopping cart</h1>
+				{cartItems.length === 0 ? (
+					<Message>
+						Your cart is empty <Link to='/'>Return</Link>
+					</Message>
+				) : (
+					<ListGroup variant='flush'>
+						{cartItems.map((item) => (
+							<ListGroup.Item key={item.product}>
+								<Row>
+									<Col md={2}>
+										<Image src={item.image} alt={item.name} fluid rounded />
+									</Col>
+									<Col md={3}>
+										<Link to={`/prouct/${item.product}`}>{item.name}</Link>
+									</Col>
+									<Col md={2}>${item.price}</Col>
+									<Col md={2}>
+										<Form.Control
+											as='select'
+											value={item.quantity}
+											onChange={(e) =>
+												dispatch(
+													addToCart(item.product, Number(e.target.value))
+												)
+											}
+										>
+											{[...Array(item.countInStock).keys()].map((count) => (
+												<option key={count + 1} value={count + 1}>
+													{count + 1}
+												</option>
+											))}
+										</Form.Control>
+									</Col>
+									<Col md={2}>
+										<Button
+											variant='light'
+											type='button'
+											onClick={removeFromCartHandler(item.product)}
+										>
+											<i className='fas fa-trash'></i>
+										</Button>
+									</Col>
+								</Row>
+							</ListGroup.Item>
+						))}
+					</ListGroup>
+				)}
+			</Col>
+
+			<Col md={4}>
+				<Card>
+					<ListGroup>
+						<ListGroup.Item>
+							<h2>
+								Subtotal(
+								{cartItems.reduce(
+									(acc, currentItem) => acc + currentItem.quantity,
+									0
+								)}
+								) items
+							</h2>
+							$
+							{cartItems
+								.reduce(
+									(acc, currentItem) =>
+										acc + currentItem.quantity * currentItem.price,
+									0
+								)
+								.toFixed(2)}
+						</ListGroup.Item>
+					</ListGroup>
+				</Card>
+			</Col>
+		</Row>
+	);
 };
 
 export default CartPage;
