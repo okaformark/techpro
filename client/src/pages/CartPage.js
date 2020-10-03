397685001291291e6f73caa5fbc037b247143b33
okaformark <okaformark@gmail.com>
2020-10-03 18:03:41 -0500

    add to cart

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -30,6 +30,10 @@ const CartPage = ({ match, location, history }) => {
 
 	const removeFromCartHandler = (id) => {};
 
+	const checkoutHandler = () => {
+		history.push('/login?redirect=shipping');
+	};
+
 	return (
 		<Row>
 			<Col md={8}>
@@ -103,6 +107,16 @@ const CartPage = ({ match, location, history }) => {
 									0
 								)
 								.toFixed(2)}
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<Button
+								type='button'
+								className='btn-block'
+								disabled={cartItems.length === 0}
+								onClick={checkoutHandler}
+							>
+								Check Out
+							</Button>
 						</ListGroup.Item>
 					</ListGroup>
 				</Card>
