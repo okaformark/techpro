778b439dacfe86764b30ea7efb5a68d1f7b5fa36
okaformark <okaformark@gmail.com>
2020-10-04 09:57:15 -0500

    getting body response on postman

---
+++ backend/controllers/userController.js
@@ -0,0 +1,25 @@
+const asyncHandler = require('express-async-handler');
+const User = require('../models/UserModel');
+
+//@desc    authenticate user and get token
+//@route    POST    api/users/login
+//@access public
+const authUser = asyncHandler(async (req, res) => {
+	const { email, password } = req.body;
+
+	const user = await User.findOne({ email });
+	if (user && (await user.matchPassword(password))) {
+		res.json({
+			_id: user._id,
+			email: user.email,
+			name: user.name,
+			isAdmin: user.isAdmin,
+			token: null,
+		});
+	} else {
+		res.sendStatus(401);
+		throw new Error('invalid. Not authorized');
+	}
+});
+
+module.exports = authUser;
