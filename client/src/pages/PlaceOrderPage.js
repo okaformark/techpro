896c33198b03af7bc2bae5cb503fb38f770b0ea8
okaformark <okaformark@gmail.com>
2020-10-06 02:01:01 -0500

    calling cart items and displaying info on place order page

--- client/src/pages/PlaceOrderPage.js
+++ client/src/pages/PlaceOrderPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../component/Message';
 import CheckoutStep from '../pages/CheckoutStep';
@@ -22,6 +23,43 @@ const PlaceOrderPage = () => {
 								{shippingAddress.city} {shippingAddress.state},{' '}
 								{shippingAddress.zipCode},{shippingAddress.country}
 							</p>
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<h2>Payment Method</h2>
+							<strong>Method: </strong>
+							{cart.paymentMethod}
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<h2>Order Items</h2>
+							{cart.cartItems.length === 0 ? (
+								<Message>Your cart is empty</Message>
+							) : (
+								<ListGroup variant='flush'>
+									{cart.cartItems.map((item, index) => (
+										<ListGroup.Item key={index}>
+											<Row>
+												<Col md={1}>
+													<Image
+														src={item.Image}
+														alt={item.name}
+														rounded
+														fluid
+													/>
+												</Col>
+												<Col>
+													<Link to={`/product/${item.product}`}>
+														{item.name}
+													</Link>
+												</Col>
+												<Col md={4}>
+													{item.quantity} x ${item.price} = $
+													{item.quatity * item.price}
+												</Col>
+											</Row>
+										</ListGroup.Item>
+									))}
+								</ListGroup>
+							)}
 						</ListGroup.Item>
 					</ListGroup>
 				</Col>
