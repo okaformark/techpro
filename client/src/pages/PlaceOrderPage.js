90a7cf0907da51d227988677ee29adf8aa969f94
okaformark <okaformark@gmail.com>
2020-10-06 02:34:04 -0500

    able to calculate total price from cart items

--- client/src/pages/PlaceOrderPage.js
+++ client/src/pages/PlaceOrderPage.js
@@ -8,12 +8,38 @@ import CheckoutStep from '../pages/CheckoutStep';
 const PlaceOrderPage = () => {
 	const cart = useSelector((state) => state.cart);
 	const { shippingAddress } = cart;
-	console.log(cart);
+
+	// gives two decimals
+	const addDecimals = (num) => {
+		return (Math.round(num * 100) / 100).toFixed(2);
+	};
+
+	cart.itemsPrice = addDecimals(
+		cart.cartItems.reduce(
+			(acc, currItem) => acc + currItem.price * currItem.quantity,
+			0
+		)
+	);
+
+	cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 10 : 5);
+
+	cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)));
+
+	cart.totalPrice = (
+		Number(cart.itemsPrice) +
+		Number(cart.shippingPrice) +
+		Number(cart.taxPrice)
+	).toFixed(2);
+
+	const placeorderHandler = () => {
+		console.log('aaaa');
+	};
+
 	return (
 		<>
 			<CheckoutStep step1 step2 step3 step4 />
 			<Row>
-				<Col>
+				<Col md={8}>
 					<ListGroup variant='flush'>
 						<ListGroup.Item>
 							<h2>Shipping</h2>
@@ -38,12 +64,12 @@ const PlaceOrderPage = () => {
 									{cart.cartItems.map((item, index) => (
 										<ListGroup.Item key={index}>
 											<Row>
-												<Col md={1}>
+												<Col md={2}>
 													<Image
-														src={item.Image}
+														src={item.image}
 														alt={item.name}
-														rounded
 														fluid
+														rounded
 													/>
 												</Col>
 												<Col>
@@ -53,7 +79,7 @@ const PlaceOrderPage = () => {
 												</Col>
 												<Col md={4}>
 													{item.quantity} x ${item.price} = $
-													{item.quatity * item.price}
+													{item.quantity * item.price}
 												</Col>
 											</Row>
 										</ListGroup.Item>
@@ -62,6 +88,50 @@ const PlaceOrderPage = () => {
 							)}
 						</ListGroup.Item>
 					</ListGroup>
+				</Col>
+				<Col md={4}>
+					<Card>
+						<ListGroup variant='flush'>
+							<ListGroup.Item>
+								<h2>Order Summary</h2>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>Items</Col>
+									<Col>${cart.itemsPrice}</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>Shipping</Col>
+
+									<Col>${cart.cartItems.length && cart.shippingPrice}</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>Tax</Col>
+									<Col>${cart.taxPrice}</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>Total</Col>
+									<Col>${cart.totalPrice}</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Button
+									type='button'
+									className='btn-block'
+									disabled={cart.cartItems === 0}
+									onClick={placeorderHandler}
+								>
+									Place Order
+								</Button>
+							</ListGroup.Item>
+						</ListGroup>
+					</Card>
 				</Col>
 			</Row>
 		</>
