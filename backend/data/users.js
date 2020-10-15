b26870d6174dda139b4f5a9ae36c8c845353ae68
okaformark <okaformark@gmail.com>
2020-10-15 17:39:16 -0500

    re-seeded data

--- backend/data/users.js
+++ backend/data/users.js
@@ -5,20 +5,33 @@ const users = [
 		firstName: 'mark',
 		lastName: 'okafor',
 		email: 'admin@admin.com',
-		password: bcrypt.hashSync('1234', 10),
+		password: bcrypt.hashSync('admin1234', 10),
 		isAdmin: true,
 	},
 	{
 		firstName: 'jon',
 		lastName: 'snow',
 		email: 'jon@yahoo.com',
-		password: bcrypt.hashSync('1234', 10),
+		password: bcrypt.hashSync('jon1234', 10),
 	},
 	{
 		firstName: 'toph',
 		lastName: 'beifong',
 		email: 'toph@gmail.com',
-		password: bcrypt.hashSync('1234', 10),
+		password: bcrypt.hashSync('toph1234', 10),
+	},
+	{
+		firstName: 'oge',
+		lastName: 'egbujor',
+		email: 'oge@gmail.com',
+		password: bcrypt.hashSync('oge1234', 10),
+		isAdmin: true,
+	},
+	{
+		firstName: 'kay',
+		lastName: 'oke',
+		email: 'kay@gmail.com',
+		password: bcrypt.hashSync('aky1234', 10),
 	},
 ];
 
