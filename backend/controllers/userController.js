754371680aeb2ef57833e1be40033ae74024d6ee
okaformark <okaformark@gmail.com>
2020-10-14 20:58:07 -0500

    adding delete route for admin to delete users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -118,10 +118,25 @@ const getUsers = asyncHandler(async (req, res) => {
 	res.json(users);
 });
 
+//@desc     delete users
+//@route    DELETE/api/users/:id
+//@access   private/admin
+const deleteUser = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.params.id);
+	if (user) {
+		await user.remove();
+		res.json('User is removed');
+	} else {
+		res.status(404);
+		throw new Error('User not found');
+	}
+});
+
 module.exports = {
 	authUser,
 	getUserProfile,
 	registerUser,
 	updateUserProfile,
 	getUsers,
+	deleteUser,
 };
--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -6,6 +6,7 @@ const {
 	registerUser,
 	updateUserProfile,
 	getUsers,
+	deleteUser,
 } = require('../controllers/userController');
 const { protect, admin } = require('../middleware/authMiddleware');
 
@@ -15,5 +16,6 @@ router
 	.get(protect, getUserProfile)
 	.put(protect, updateUserProfile);
 router.route('/').post(registerUser).get(protect, admin, getUsers);
+router.route('/:id').delete(protect, admin, deleteUser);
 
 module.exports = router;
