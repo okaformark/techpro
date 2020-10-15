4990c6675a541004d0edb152b88dd04e0cd010ab
okaformark <okaformark@gmail.com>
2020-10-15 16:58:49 -0500

    product deleted from DB

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,4 +22,18 @@ const getProductById = asyncHandler(async (req, res) => {
 	}
 });
 
-module.exports = { getProducts, getProductById };
+//@desc		delete a product --admin
+//@route	DELETE/api/products/:id
+//@acess	private/admin
+const deleteProduct = asyncHandler(async (req, res) => {
+	const product = await Product.findById(req.params.id);
+
+	if (product) {
+		await product.remove();
+		res.json({ message: 'Product removed' });
+	} else {
+		res.status(404);
+		throw new Error('Not Found');
+	}
+});
+module.exports = { getProducts, getProductById, deleteProduct };
